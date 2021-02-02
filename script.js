6e0eded47f8daa769e276b181a88daf593698667
suruchi2402 <pinkp7840@gmail.com>
2021-02-02 08:14:36 +0530

    Add file via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    d = new Date();
+    //d grabs the current date
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform = `rotate(${mrotation}deg)`;
+    sec.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
